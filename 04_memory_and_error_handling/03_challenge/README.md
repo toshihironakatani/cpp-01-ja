fd7d466babe7b5599463b0fb8a5fe8070486e9c6
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2024-03-01 14:22:56 +0900

    Update challenges 04_03

--- 04_memory_and_error_handling/03_challenge/README.md
+++ 04_memory_and_error_handling/03_challenge/README.md
@@ -2,15 +2,3 @@
 
 `std::vector<int>` を引数にとり、各要素の値を2倍にした新しい `std::vector<int>` を返す関数 `double_elements` を作成してください。次に、範囲ベースのforループを使用して、元のvectorと新しいvectorの両方の要素を出力します。
 
-```cpp
-#include <iostream>
-#include <vector>
-
-// コードを入力してください
-
-int main() {
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
-
-    // コードを入力してください
-}
-```
--- 04_memory_and_error_handling/03_challenge/tests/tests.cpp
+++ 04_memory_and_error_handling/03_challenge/tests/tests.cpp
@@ -1,19 +1,12 @@
 #include "tests.hpp"
+
 #include <gtest/gtest.h>
-#include <string>
-#include <vector>
 
-int dummy_main();
+#include "elements.hpp"
 
-TEST(Test, Compilation) {
-    bool pass { !dummy_main() };
-    ASSERT_EQ(pass, true);
-}
-
-extern std::vector<int> numbers;
-std::vector<int> double_elements(const std::vector<int>& c);
 
 TEST(Test, DoubleElements) {
+    std::vector<int> numbers = {1, 2, 4, 6, 7};
     auto doubled = double_elements(numbers);
     for (int i = 0; i < doubled.size(); ++i) {
         ASSERT_EQ(doubled[i], numbers[i] * 2);
