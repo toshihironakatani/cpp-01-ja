be2d0c3c2e3a10e35c7ba173d27a25d35d882057
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2024-03-01 14:46:47 +0900

    Update challenges 04_04

--- 04_memory_and_error_handling/04_challenge/README.md
+++ 04_memory_and_error_handling/04_challenge/README.md
@@ -2,16 +2,3 @@
 
 `std::vector<int>` と整数型のインデックスを引数にとる関数 `element_at` を作成してください。この関数では、指定したインデックスにあるvectorの要素を返し、インデックスが範囲外の場合は適切なエラーメッセージとともに `std::out_of_range` 例外をスローするようにします。main関数で有効なインデックスと無効なインデックスの両方を指定して `element_at` を呼び出し、例外を適切に処理してください。
 
-```cpp
-#include <iostream>
-#include <stdexcept>
-#include <vector>
-
-// コードを入力してください
-
-int main() {
-    std::vector<int> numbers = {1, 3, 5, 7, 9};
-
-    // コードを入力してください
-}
-```
--- 04_memory_and_error_handling/04_challenge/tests/tests.cpp
+++ 04_memory_and_error_handling/04_challenge/tests/tests.cpp
@@ -1,39 +1,30 @@
 #include "tests.hpp"
 #include <gtest/gtest.h>
-#include <string>
-#include <vector>
 
-int dummy_main();
-
-TEST(Test, Compilation) {
-    bool pass { !dummy_main() };
-    ASSERT_EQ(pass, true);
-}
-
-extern std::vector<int> numbers;
-int element_at(const std::vector<int>& c, int index);
+#include "elements.hpp"
 
 void testElementAt(const std::vector<int>& n, int index) {
-    try {
-        auto element = element_at(numbers, index);
-        ASSERT_EQ(numbers[index], element);
-    } catch (...) {
-        ASSERT_TRUE(false);
-    }
+    std::vector<int> numbers = {1, 3, 4, 5};
+    auto element = element_at(numbers, index);
+    ASSERT_EQ(numbers[index], element);
 }
 
 TEST(Test, ElementAtZero) {
+    std::vector<int> numbers = {1, 3, 4, 5};
     testElementAt(numbers, 0);
 }
 
 TEST(Test, ElementAtSizeMinusOne) {
+    std::vector<int> numbers = {1, 3, 4, 5};
     testElementAt(numbers, static_cast<int>(numbers.size() - 1));
 }
 
 TEST(Test, ElementAtNegativeOne) {
+    std::vector<int> numbers = {1, 3, 4, 5};
     ASSERT_THROW(element_at(numbers, -1), std::out_of_range);
 }
 
 TEST(Test, ElementAtSize) {
+    std::vector<int> numbers = {1, 3, 4, 5};
     ASSERT_THROW(element_at(numbers, static_cast<int>(numbers.size())), std::out_of_range);
 }
